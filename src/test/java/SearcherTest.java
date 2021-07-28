dbf63522c66b073e1a058d5a19cb6889b0061140
Abolfazl <g.o.d.abolfazlm@gmail.com>
2021-07-28 13:45:02 +0430

    normal word and words test, files in test resources edited

--- src/test/java/SearcherTest.java
+++ src/test/java/SearcherTest.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.List;
 
 
 class SearcherTest {
@@ -12,19 +12,31 @@ class SearcherTest {
         invertedIndex = new InvertedIndex();
         searcher = new Searcher();
         invertedIndex.indexAllFiles("src/test/java/TestResources/EnglishData");
+        searcher.setInvertedIndex(invertedIndex);
     }
     @Test
-    void normalWordsSearch(){
+    void normalWordSearch(){
+        List<WordInfo> result = searcher.search("ali");
+       Assertions.assertEquals(7, result.size());
+       int i = 1;
+        for (WordInfo wordInfo : result) {
+            Assertions.assertEquals(i + "", wordInfo.getFileName());
+            i++;
+        }
+    }
 
+    @Test
+    void normalWordsSearch(){
+        List<WordInfo> result = searcher.search("ali va hasan godratmand");
+        Assertions.assertEquals(1, result.size());
+        Assertions.assertEquals("3", result.get(0).getFileName());
     }
     @Test
     void deleteMinusWordsFromEmptyResultTest() {
-        searcher.setInvertedIndex(invertedIndex);
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
         outContent.reset();
         Assertions.assertDoesNotThrow(() -> searcher.search("-kid"));
-
         assert(outContent.toString().contains("please try a different keyword for your search!"));
     }
 }
