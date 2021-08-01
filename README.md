80cb4fb8d533cf75ddb18f78541348b37b559cb5
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-08-01 13:38:59 +0430

    delete java files

--- README.md
+++
@@ -1,4 +0,0 @@
-# Summer1400-SE-Team 22
-https://github.com/Star-Academy/codestar-intern-issues/issues/259
-
-https://github.com/Star-Academy/codestar-internship/blob/master/Projects/Phase01-Search/Phase01-Search.md
--- WordInfo.java
+++
@@ -1,9 +0,0 @@
-public class WordInfo {
-    public String fileName;
-    public int position;
-
-    public WordInfo(String fileName, int position) {
-        this.fileName = fileName;
-        this.position = position;
-    }
-}
