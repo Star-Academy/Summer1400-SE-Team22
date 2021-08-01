e481c39a01fb76fd6d0956a90f2c585377ef207d
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-08-01 13:48:44 +0430

    m

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Summer1400_SE_Team22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
