71cc91f72e5f73f99613f88c78849e5f41868b23
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-08-01 15:26:30 +0430

    Co-authored-by: Abolfazl <Morovati306@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Summer1400_SE_Team22
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");  
         }
     }
 }
