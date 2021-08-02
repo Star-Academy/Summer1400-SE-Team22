b4e9aa122050a934c063a92b8ebcb791de8ada9e
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-08-02 12:36:27 +0430

    Co-authored-by: Abolfazl <Morovati306@users.noreply.github.com>

--- Student.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Summer1400_SE_Team22
-{
-    public class Student
-    {
-         public int StudentNumber {get; set;}
-         public string FirstName {get; set;}
-         public string LastName {get; set;} 
-    }
-}
