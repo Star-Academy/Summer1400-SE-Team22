1227f39d66e9257a6d1d6cf96c9700ade19acdf8
Abolfazl <g.o.d.abolfazlm@gmail.com>
2021-08-03 11:44:41 +0430

    cleanup

--- Program.cs
+++ Program.cs
@@ -17,30 +17,23 @@ namespace Summer1400_SE_Team22
             string scoresJson = File.ReadAllText("Database/Scores.json");
             LessonScore[] scores = JsonSerializer.Deserialize<LessonScore[]>(scoresJson);
             
-            
             foreach (var x in scores) students[x.StudentNumber - 1].AddAnScore(x.Score);
 
-            
             CalculateGPA(students);
 
-            List<Student> studentsList = students.ToList().OrderByDescending(student => student.GPA).ToList();
-
-            PrintResult(studentsList);
+            PrintResult(students.ToList().OrderByDescending(student => student.GPA).ToList()
+);
         }
 
         private static void PrintResult(List<Student> studentsList){
-            for (int i = 0; i < 3; i++)
-            {
-                Console.WriteLine("Rank: " + (i+1) + ", name: " + studentsList.ElementAt(i).FirstName + " " + studentsList.ElementAt(i).LastName + ", GPA: " + studentsList.ElementAt(i).GPA.ToString("N2")) ;
-            }
+            for (int i = 0; i < 3; i++)  Console.WriteLine("Rank: " + (i+1) + ", name: " + studentsList.ElementAt(i).FirstName + " " + studentsList.ElementAt(i).LastName + ", GPA: " + studentsList.ElementAt(i).GPA.ToString("N2")) ;
         }
 
         private static void CalculateGPA(Student[] students){
             for (int i = 0; i < students.Length; i++)
             {
                 double sumOfGrades = 0;
-                foreach (var x in students[i].Scores) sumOfGrades += x;
-                
+                foreach (var x in students[i].Scores) sumOfGrades += x; 
                 students[i].GPA = (double)(sumOfGrades / students[i].Scores.Count);
             }          
         }
