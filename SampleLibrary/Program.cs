010933ab77c4ccc4e50472323fcf6f7f9717692e
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-08-06 22:18:08 +0430

    setup a simple .net project

---
+++ SampleLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a git action test!");
+        }
+    }
+}
