0531a9f6cc840516134ddfc2cd569790ead35095
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-08-07 13:15:07 +0430

    use library functions & fix bugs

--- Program.cs
+++ Program.cs
@@ -9,54 +9,57 @@ namespace Summer1400_SE_Team22
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            string studentsJson = File.ReadAllText("Database/Students.json");
-            Student[] students = JsonSerializer.Deserialize<Student[]>(studentsJson);
+            var studentsJson = File.ReadAllText("Database/Students.json");
+            var students = JsonSerializer.Deserialize<Student[]>(studentsJson);
 
-            string scoresJson = File.ReadAllText("Database/Scores.json");
-            LessonScore[] scores = JsonSerializer.Deserialize<LessonScore[]>(scoresJson);
-            
+            var scoresJson = File.ReadAllText("Database/Scores.json");
+            var scores = JsonSerializer.Deserialize<LessonScore[]>(scoresJson);
+
             foreach (var x in scores) students[x.StudentNumber - 1].AddAnScore(x.Score);
 
             CalculateGPA(students);
 
-            PrintResult(students.ToList().OrderByDescending(student => student.GPA).ToList()
-);
+            PrintResult(students.ToList().OrderByDescending(student => student.GPA).ToList());
+
         }
 
-        private static void PrintResult(List<Student> studentsList){
-            for (int i = 0; i < 3; i++)  Console.WriteLine("Rank: " + (i+1) + ", name: " + studentsList.ElementAt(i).FirstName + " " + studentsList.ElementAt(i).LastName + ", GPA: " + studentsList.ElementAt(i).GPA.ToString("N2")) ;
+        private static void PrintResult(List<Student> studentsList)
+        {
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine("Rank: " + (i + 1) + ", name: " + studentsList.ElementAt(i).FirstName + " " +
+                                  studentsList.ElementAt(i).LastName + ", GPA: " +
+                                  studentsList.ElementAt(i).GPA.ToString("N2"));
         }
 
-        private static void CalculateGPA(Student[] students){
-            for (int i = 0; i < students.Length; i++)
+        private static void CalculateGPA(IEnumerable<Student> students)
+        {
+            foreach (var t in students)
             {
-                double sumOfGrades = 0;
-                foreach (var x in students[i].Scores) sumOfGrades += x; 
-                students[i].GPA = (double)(sumOfGrades / students[i].Scores.Count);
-            }          
+                t.GPA = t.Scores.Average();
+            }
         }
     }
 
     public class Student
     {
-         public int StudentNumber {get; set;}
-         public string FirstName {get; set;}
-         public string LastName {get; set;} 
-         public List<double> Scores = new List<double>();
-         public double GPA {get; set;} = 0 ;
+        public int StudentNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<double> Scores = new List<double>();
+        public double GPA { get; set; } = 0;
 
-         public void AddAnScore(double score){
-             Scores.Add(score);
-         }
+        public void AddAnScore(double score)
+        {
+            Scores.Add(score);
+        }
     }
 
     public class LessonScore
     {
-      public int StudentNumber {get; set;}
-      public string Lesson {get; set;}
-      public double Score {get; set;}
+        public int StudentNumber { get; set; }
+        public string Lesson { get; set; }
+        public double Score { get; set; }
     }
-
 }
