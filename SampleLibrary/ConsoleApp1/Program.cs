138c7aff88d06ef803ece90e9f61562e8f77649b
Abolfazl Ghalandary <ab.ghalandary@outlook.com>
2021-08-09 15:15:51 +0430

    add a console project for debug & set database

---
+++ SampleLibrary/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SampleLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _invertedIndex = new InvertedIndex();
+            var _searcher = new Searcher();
+            _invertedIndex.IndexAllFiles("EnglishData");
+        }
+    }
+}
